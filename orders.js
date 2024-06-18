b8ea8019a7f9d7315b533e9cac039b0f11c81039
Mande-yang <59284141+yangsqxglc@users.noreply.github.com>
2024-06-18 15:29:46 +0800

    2024年6月18日更新

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: '0001',
+        productNumber: 'yangsq',
+        paymentStatus: '10:00',
+        status: 'Windows'
+    },
+    {
+        productName: '0123',
+        productNumber: 'wither',
+        paymentStatus: '11:25',
+        status: 'Android'
+    },
+    {
+        productName: '01125',
+        productNumber: 'Janna',
+        paymentStatus: '12:55',
+        status: 'ios'
+    },
+]
